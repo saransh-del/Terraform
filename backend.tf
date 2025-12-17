1d851e94745c6a3b2ca8e9740b16d3fa637844ba
saransh <saranshgumasta@gmail.com>
2025-12-17 16:45:12 +0530

    backend.tf is updated

--- backend.tf
+++ backend.tf
@@ -1,13 +1,11 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-saransh4326"
-    key            = "dev/terraform.tfstate"
+    key            = "terraform-2/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
-    assume_role {
-      role_arn = "arn:aws:iam::353284254141:role/TerraformStateBackendRole"
-    }
+    role_arn       = "arn:aws:iam::353284254141:role/TerraformStateBackendRole"
   }
 }
 

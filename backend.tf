2a4d9331690afc4b9ae8862a969a7fa3a3c920d9
saransh <saranshgumasta@gmail.com>
2025-12-18 14:21:31 +0530

    updated files

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-saransh4326"
-    key            = "terraform-2/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket  = "terraform-state-saransh4326"
+    key     = "terraform-2/dev/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "terraform-backend"
+    use_lockfile = true
   }
 }
